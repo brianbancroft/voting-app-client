b8abe6cdc2aac83935d2a49c9e22fa581776ca0f
Brian Bancroft <hello@brianbancroft.ca>
2020-01-11 14:57:43 -0800

    🚧  Build basic vote interface

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,13 +1,52 @@
-import React from 'react'
-import { GenericContainer, SampleWidget } from '..'
+import React, { useState } from 'react'
+import { Box, Button, Header, Grid, Footer, Main } from 'grommet'
 
 const Layout = () => {
+  const [canVote, setCanVote] = useState(true)
+  const [choice, setChoice] = useState(null)
+
+  const vote = choice => {
+    setCanVote(false)
+    setChoice(choice)
+  }
+
   return (
-    <>
-      <div>Hello new app</div>
-      <GenericContainer>box</GenericContainer>
-      <SampleWidget />
-    </>
+    <Main>
+      <Grid rows={['xxsmall', '1fr', 'xsmall']} fill="vertical">
+        <Header background="highlight">Votijng app</Header>
+        <Box border={{ size: 'small', color: 'highlight' }} pad="large">
+          {choice && <Box>Vote Choice: {choice} </Box>}
+          <Box
+            justify="between"
+            border={{ size: 'small', color: 'dark-2' }}
+            direction="row"
+            width="large"
+          >
+            <Button onClick={() => vote('Choice A')} disabled={!canVote}>
+              <Box
+                height="xsmall"
+                width="small"
+                background="accent-2"
+                border="dark-1"
+              >
+                Choice A
+              </Box>
+            </Button>
+            <Button onClick={() => vote('Choice B')} disabled={!canVote}>
+              <Box
+                height="xsmall"
+                width="small"
+                background="accent-3"
+                border="dark-1"
+              >
+                Choice B
+              </Box>
+            </Button>
+          </Box>
+        </Box>
+        <Footer background="brand">Learn More</Footer>
+      </Grid>
+    </Main>
   )
 }
 
