0fc3da3dacdb38087fd95847e4c7330f32447e93
Brian Bancroft <hello@brianbancroft.ca>
2020-01-12 13:29:18 -0800

    ✨  Add question to list of questions

--- src/components/PageAdmin/PageAdmin.js
+++ src/components/PageAdmin/PageAdmin.js
@@ -1,22 +1,93 @@
-import React, { useState } from 'react'
-import { Box, Text, Heading } from 'grommet'
+import React, { Component } from 'react'
+import { Box, Text, Heading, Form, FormField, Button } from 'grommet'
 import { ConfigStageDisplay } from '..'
 
-const PageAdmin = () => {
-  const [setupStage, setSetupStage] = useState(0)
-  const stages = ['Set up questions', 'Voting']
+class PageAdmin extends Component {
+  constructor() {
+    super()
+    this.state = {
+      questionList: [
+        {
+          question: 'Text for the first question',
+          answers: ['Answer 1', 'Answer 2'],
+        },
+        {
+          question: 'Text for the second question',
+          answers: ['Answer 1', 'Answer 2'],
+        },
+      ],
+    }
+  }
 
-  return (
-    <>
-      <Box direction="column">
-        <Box>
-          <Heading>Voting</Heading>
+  addQuestion = () => {
+    const { questionList } = this.state
+
+    this.setState({
+      questionList: [
+        ...questionList,
+        {
+          question: 'What is the question?',
+          answers: ['Answer 1', 'Answer 2'],
+        },
+      ],
+    })
+  }
+
+  render() {
+    const { questionList } = this.state
+
+    return (
+      <>
+        <Box direction="column">
+          <Box>
+            <Heading level={3}>Voting</Heading>
+          </Box>
+          <ConfigStageDisplay />
+          <Box>
+            <Form
+              onSubmit={e => {
+                console.log('Form submitted3')
+              }}
+            >
+              <Box direction="row" overflow={{ horizontal: 'scroll' }}>
+                {questionList.map(({ question, answers }) => (
+                  <Box className="question-element">
+                    <Box width="medium">
+                      <FormField label={question} />
+                    </Box>
+                    {answers.map(answer => (
+                      <Box width="small">
+                        <FormField label={answer} />
+                      </Box>
+                    ))}
+
+                    <Box direction="row">
+                      <Box width="small">
+                        <FormField label="(disabled) Another Answer" disabled />
+                      </Box>
+                      <Button
+                        onClick={() => {
+                          console.log('Add answer selected')
+                        }}
+                      >
+                        Add Answer
+                      </Button>
+                    </Box>
+                  </Box>
+                ))}
+
+                <Box background="brand">
+                  <Button onClick={this.addQuestion}>
+                    <Text>Add Question</Text>
+                  </Button>
+                </Box>
+              </Box>
+            </Form>
+          </Box>
         </Box>
-        <ConfigStageDisplay />
-        <Box>Form goes here</Box>
-      </Box>
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default PageAdmin
