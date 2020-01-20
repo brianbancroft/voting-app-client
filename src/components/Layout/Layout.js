5159908b3ef4a853a74f49c2fe99ba083369f279
Brian Bancroft <hello@brianbancroft.ca>
2020-01-20 13:43:18 -0800

    🔥  Remove header and footer from layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,26 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Header, Grid, Footer, Main, Text, Box } from 'grommet'
-import { Link } from 'react-router-dom'
+import { Main, Box } from 'grommet'
 
 const Layout = ({ children }) => (
   <>
     <Main>
-      <Grid rows={['xxsmall', '1fr', 'xsmall']} fill="vertical">
-        <Header background="highlight">
-          <Box pad="medium">
-            <Link to="/">
-              <Text>Voting App</Text>
-            </Link>
-          </Box>
-        </Header>
-        <Box overflow={{ vertical: 'scroll' }}>{children}</Box>
-        <Footer background="brand">
-          <Box pad="medium">
-            <Link to="/about">Learn More</Link>
-          </Box>
-        </Footer>
-      </Grid>
+      <Box overflow={{ vertical: 'scroll' }}>{children}</Box>
     </Main>
   </>
 )
